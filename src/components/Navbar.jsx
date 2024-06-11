dfd8f5198b35aeb82f3c96ce385d7b104e36fc99
Dimitri Chupryna <dmytro.chupryna.1@gmail.com>
2024-06-11 11:27:03 +0100

    Navbar light mode

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,18 @@
+import logo from '../assets/react.svg';
+
 export default function Navbar() {
     return (
-        <>
-        hello from Navbar
-        </>    )
+        <nav>
+            <img src={logo} alt="logo" className='nav--logo'/>
+            <h3 className="nav--title">ReactFacts</h3>
+            <div className='toggler'>
+                <p className='toggler--light'>Light</p>
+                <div className="toggler--slider">
+                    <div className="toggler--slider--circle"></div>
+                </div>
+                <p className='toggler--dark'>Dark</p>
+            </div>      
+        </nav>    
+        
+    )
 }
