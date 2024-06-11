ef0b6792c75e4fee19add7127662be1eb295733b
Dimitri Chupryna <dmytro.chupryna.1@gmail.com>
2024-06-11 11:36:41 +0100

    darkMode

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,13 @@
 import logo from '../assets/react.svg';
 
-export default function Navbar() {
+export default function Navbar(props) {
     return (
-        <nav>
+        <nav className={props.darkMode ? 'dark' : ''}>
             <img src={logo} alt="logo" className='nav--logo'/>
             <h3 className="nav--title">ReactFacts</h3>
             <div className='toggler'>
                 <p className='toggler--light'>Light</p>
-                <div className="toggler--slider">
+                <div className="toggler--slider" onClick={props.toggleDarkMode}>
                     <div className="toggler--slider--circle"></div>
                 </div>
                 <p className='toggler--dark'>Dark</p>
